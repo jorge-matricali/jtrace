55fb325f34226a8fbb57f6152d3afda539ab16cb
Jorge Matricali <jorgematricali@gmail.com>
2014-02-16 00:09:23 -0300

    + Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+[![Version 2.3.0](https://img.shields.io/badge/version-v0.0.1-green.svg)](:release:)
+[![Build Status](https://travis-ci.org/jorge-matricali/jtrace.svg?branch=master)](:status:)
+[![MIT licensed](https://img.shields.io/github/license/jorge-matricali/jtrace.svg)](https://raw.githubusercontent.com/jorge-matricali/jtrace/master/LICENSE.md) [![Packagist](https://img.shields.io/packagist/dt/jorge-matricali/jtrace.svg)](https://packagist.org/packages/jorge-matricali/jtrace)
+
+# jTrace PHP
+Prints PHP exceptions in a Java style :)
+
+## Installation
+
+You can add this library as a dependency to your project using [Composer](https://getcomposer.org/):
+
+    composer require jorge-matricali/jtrace
+
+If you only need this library during development, then you should install using:
+
+    composer require --dev jorge-matricali/jtrace
+
+### Usage
+
+```php
+use JorgeMatricali\JTrace;
+
+try {
+    // ...
+} catch (Exception $e) {
+    echo JTrace::print($e);
+}
+```
+
+The code above yields the output below:
+
+Phalcon\Mvc\Dispatcher\Exception: IndexController handler class cannot be loaded at Phalcon.Mvc.Dispatcher._throwDispatchException(index.php:93) at Phalcon.Dispatcher._dispatch(Unknown Source) at Phalcon.Dispatcher.dispatch(Unknown Source) at Phalcon.Mvc.Application.handle(Unknown Source) ... 1 more
---
+++ src/JTrace.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+MIT License
+
+Copyright (c) 2014 Jorge Matricali
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace JorgeMatricali\JTrace;
+
+class JTrace
+{
+    /**
+    * Prints a Java style exception trace
+    * @param $exception
+    * @param $seen
+    * @return array of strings, one entry per trace line
+    */
+    public static function printException($e, $seen = null)
+    {
+        $starter = $seen ? 'Caused by: ' : '';
+        $result = array();
+        if (!$seen) {
+            $seen = array();
+        }
+        $trace = $e->getTrace();
+        $prev = $e->getPrevious();
+        $result[] = sprintf('%s%s: %s', $starter, get_class($e), $e->getMessage());
+        $file = $e->getFile();
+        $line = $e->getLine();
+        while (true) {
+            $current = "$file:$line";
+            if (is_array($seen) && in_array($current, $seen)) {
+                $result[] = sprintf(' ... %d more', count($trace)+1);
+                break;
+            }
+            $result[] = sprintf(
+                ' at %s%s%s(%s%s%s)',
+                count($trace) && array_key_exists('class', $trace[0]) ? str_replace('\\', '.', $trace[0]['class']) : '',
+                count($trace) && array_key_exists('class', $trace[0]) && array_key_exists('function', $trace[0]) ? '.' : '',
+                count($trace) && array_key_exists('function', $trace[0]) ? str_replace('\\', '.', $trace[0]['function']) : '(main)',
+                $line === null ? $file : basename($file),
+                $line === null ? '' : ':',
+                $line === null ? '' : $line
+            );
+            if (is_array($seen)) {
+                $seen[] = sprintf('%s:%d', $file, $line);
+            }
+            if (!count($trace)) {
+                break;
+            }
+            $file = array_key_exists('file', $trace[0]) ? $trace[0]['file'] : 'Unknown Source';
+            $line = array_key_exists('file', $trace[0]) && array_key_exists('line', $trace[0]) && $trace[0]['line'] ? $trace[0]['line'] : null;
+            array_shift($trace);
+        }
+        $result = join(PHP_EOL, $result);
+        if ($prev) {
+            $result  .= PHP_EOL . jTraceEx($prev, $seen);
+        }
+
+        return $result;
+    }
+}
